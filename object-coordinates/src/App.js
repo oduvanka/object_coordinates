aabc10fa8f9cf00fc22c899b3653bfbef3392bcf
oduvanka <qcatjul@gmail.com>
2019-07-01 22:41:11 +0400

    Создаёт пустую заготовку

---
+++ object-coordinates/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <p>
+          blank
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
