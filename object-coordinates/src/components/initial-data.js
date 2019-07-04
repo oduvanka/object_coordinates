19f96f8b14572a97628b904703774f899a686206
oduvanka <qcatjul@gmail.com>
2019-07-04 23:32:23 +0400

    Убирает большое поле ввода, добавляет строку для списка полей и кнопки + и -

--- object-coordinates/src/components/initial-data.js
+++ object-coordinates/src/components/initial-data.js
@@ -6,10 +6,24 @@ class InitialData extends React.Component {
     return (
       <form className="form-coordinates" onSubmit={this.props.jsonCoords}>
         <p className="form-group">
-          <label>JSON с координатами:</label>
-          <textarea name="coordinates" required></textarea>
+          <label>Имя:</label>
+          <input name="nameJSON" type="text" required></input>
         </p>
-        <Btn type="submit" text="Обработать" />
+        <div className="form-item" name="coordinates">
+          <p className="form-group">
+            <label>Широта:</label>
+            <input name="LatitudeJSON" type="text" required></input>
+          </p>
+          <p className="form-group">
+            <label>Долгота:</label>
+            <input name="LongitudeJSON" type="text" required></input>
+          </p>
+        </div>
+        <div>
+          <Btn type="button" text="+" title="Добавляет новую строку координат"/>
+          <Btn type="button" text="-" title="Удаляет последнюю строку координат"/>
+          <Btn type="submit" text="Обработать" />
+        </div>
       </form>
     );
   }
