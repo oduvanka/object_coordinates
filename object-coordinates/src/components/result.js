34852fda49128ea85c62a9bff42abe9de035c62a
oduvanka <qcatjul@gmail.com>
2019-07-10 18:29:57 +0400

    Добавляет заготовку сортировки

--- object-coordinates/src/components/result.js
+++ object-coordinates/src/components/result.js
@@ -5,6 +5,17 @@ class Result extends React.Component {
   render() {
     return (
       <div className="result">
+        <fieldset>
+          <legend>Сортировать: </legend>
+          <p>
+            <input type="radio" name="sort" value="sort-latitude" />
+            <label>По широте</label>
+          </p>
+          <p>
+            <input type="radio" name="sort" value="sort-longitude" />
+            <label>По долготе</label>
+          </p>
+        </fieldset>
         <Table 
           arrCoords={this.props.arrCoords}
         />
